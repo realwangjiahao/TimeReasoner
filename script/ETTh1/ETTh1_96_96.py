608fdf9bc8536f7e06d7e556a0253accefd4bdd4
Wang Jiahao <148034922+realwangjiahao@users.noreply.github.com>
2025-06-02 16:25:25 +0800

    Add files via upload

--- script/ETTh1/ETTh1_96_96.py
+++ script/ETTh1/ETTh1_96_96.py
@@ -1,1 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
+from main.ETTh.ETTh_main_one_shot_reasoning import ETTh_main_eng_short
 
+
+data_name = 'ETTh1'
+look_back, pred_window = 96, 96
+api_key = ''
+
+
+def run_task(i, attr):
+    ETTh_main_eng_short(data_name, attr, look_back, pred_window, i, api_key=api_key)
+
+tasks = [(i, attr) for i in range(10) for attr in ['HUFL', 'HULL', 'MUFL', 'MULL', 'LUFL', 'LULL', 'OT']]
+
+with ThreadPoolExecutor(max_workers=20) as executor:
+    executor.map(lambda p: run_task(*p), tasks)
+  
